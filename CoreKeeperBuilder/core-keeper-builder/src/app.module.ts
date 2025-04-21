b8e3f9dc6037858d03c35c83be65f250bd83b8c0
patulakt <patulakt@colostate.edu>
2025-04-21 12:50:49 -0600

    testable poc

--- CoreKeeperBuilder/core-keeper-builder/src/app.module.ts
+++ CoreKeeperBuilder/core-keeper-builder/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CalculatorModule } from './calculator.module';
-import { DatabaseModule } from './database.module'; 
+import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [
-      DatabaseModule,   
-      CalculatorModule,   
-    ],
-  controllers: [AppController], 
-  providers: [AppService],     
+     DatabaseModule,
+     CalculatorModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
